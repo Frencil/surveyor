4cfd861525824c682db9d377ef2feab4c53b9721
Frencil <frencils@gmail.com>
2015-07-22 11:11:38 -0600

    expand schema to more complete MVP function set

--- schema.sql
+++ schema.sql
@@ -1,6 +1,35 @@
+drop table if exists surveys;
+create table surveys (
+  id integer primary key autoincrement,
+  title text not null,
+  text text not null,
+  is_open integer not null default 0,
+  date_created datetime default current_timestamp,
+  date_opened datetime default null,
+  date_closed datetime default null
+);
+
 drop table if exists questions;
 create table questions (
   id integer primary key autoincrement,
   title text not null,
-  text text not null
+  text text not null,
+  foreign key(surveys_id) references surveys(id)
+);
+
+drop table if exists options;
+create table options (
+  id integer primary key autoincrement,
+  questions_id integer not null,
+  text text not null,
+  foreign key(questions_id) references questions(id)
+);
+  
+drop table if exists responses;
+create table responses (
+  id integer primary key autoincrement,
+  options_id integer not null,
+  text text not null,
+  date_created datetime default current_timestamp,
+  foreign key(options_id) references options(id)
 );
